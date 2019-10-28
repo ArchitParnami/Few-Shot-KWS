a29f93a39e5784c2545b242dfdcdf1aede131350
Archit Parnami <architparnami@gmail.com>
2019-10-28 18:31:38 -0400

    train params

--- scripts/train/few_shot/fewshotspeech/train.sh
+++ scripts/train/few_shot/fewshotspeech/train.sh
@@ -6,7 +6,7 @@ id=$4
 args="../run_train.py \
 		--data.dataset=googlespeech --data.way=$way --data.shot=$shot \
 		--data.query=5 --data.test_way=$way --data.test_shot=$shot --data.test_query=15 \
-		--data.train_episodes=100 --data.test_episodes=100 \
+		--data.train_episodes=200 --data.test_episodes=100 \
 		--model.model_name=protonet_conv --model.x_dim=1,51,40 --model.hid_dim=64 --model.z_dim=64 \
 		--train.epochs=200 --train.optim_method=Adam --train.learning_rate=0.001 --train.decay_every=20 \
 		--train.weight_decay=0.0 --train.patience=200  \
