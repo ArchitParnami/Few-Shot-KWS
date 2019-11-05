3c0b2d7d8c94ecdabdf364f50b019aaab705949b
Archit Parnami <architparnami@gmail.com>
2019-11-05 10:23:56 -0500

    cnn_trad_fpool

--- protonets/models/encoder/GoogleKWS.py
+++ protonets/models/encoder/GoogleKWS.py
@@ -18,10 +18,49 @@ class cnn_trad_fpool3(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU())
 
+        lin = nn.Sequential(
+            nn.Linear(11776, 32),
+            nn.ReLU()
+        )
+
+        dnn = nn.Sequential(
+            nn.Linear(32, 128),
+            nn.Sigmoid()
+        )
+
         self.encoder = nn.Sequential(
             conv1,
             conv2,
-            Flatten()
+            Flatten(),
+            lin,
+            dnn
+        )
+    
+    def forward(self, x):
+        out = self.encoder(x)
+        return out
+
+
+class cnn_trad_fpool3_simple(nn.Module):
+    def __init__(self, in_channels, hid_channels, out_channels):
+        super(cnn_trad_fpool3_simple, self).__init__()
+        conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=hid_channels,
+                    kernel_size=(20,8),stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(1,3)))
+        
+        conv2  = nn.Sequential(
+            nn.Conv2d(in_channels=hid_channels, out_channels=hid_channels, 
+                    kernel_size=(10,4), stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU())
+
+        self.encoder = nn.Sequential(
+            conv1,
+            conv2,
+            Flatten(),
         )
     
     def forward(self, x):
