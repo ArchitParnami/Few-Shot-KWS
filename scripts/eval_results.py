d3a5d3eb58b528f3093556556ea52dea5e0dd86b
Archit Parnami <architparnami@gmail.com>
2019-12-04 12:36:15 -0500

    evaluate models

---
+++ scripts/eval_results.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from predict.few_shot.eval import main
+import argparse
+
+def eval_model(model_path):
+    args = {'model.model_path' : model_path}
+    main(args)
+
+def evaluate(root_dir):
+    dirs = sorted(os.listdir(root_dir))
+    for i, result_dir in enumerate(dirs):
+        result_path = os.path.join(root_dir, result_dir)
+        timestamp = os.listdir(result_path)[0]
+        model_path = os.path.join(result_path, timestamp, 'best_model.pt')
+        eval_model(model_path)
+
+if __name__ == '__main__':
+    result_root = sys.argv[1]
+    evaluate(result_root)
+
+
